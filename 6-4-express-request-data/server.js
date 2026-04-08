d6c58d14fe260aed538bb210656f240a48e667b1
suleiman-MBS <149168290+suleiman-MBS@users.noreply.github.com>
2026-04-08 08:32:23 +0300

    Update server.js

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -126,3 +126,48 @@ LAB SETUP INSTRUCTIONS
 // Start the server by listening
 
 
+
+import express from "express";
+
+// TODO-1: Create express app instance and start server
+const app = express();
+const PORT = 3000;
+
+// TODO-2: /echo route
+app.get("/echo", (req, res) => {
+  const { name, age } = req.query;
+
+  if (!name || !age) {
+    return res.status(400).json({ ok: false, error: "name & age required" });
+  }
+
+  res.json({ ok: true, name, age, msg: `Hello ${name}, you are ${age}` });
+});
+
+// TODO-3: /profile/:first/:last route
+app.get("/profile/:first/:last", (req, res) => {
+  const { first, last } = req.params;
+  res.json({ ok: true, fullName: `${first} ${last}` });
+});
+
+// TODO-4: Param middleware
+app.param("userId", (req, res, next, userId) => {
+  const numericId = Number(userId);
+
+  if (numericId <= 0 || isNaN(numericId)) {
+    return res.status(400).json({ ok: false, error: "userId must be positive number" });
+  }
+
+  req.userIdNum = numericId;
+  next();
+});
+
+// TODO-5: /users/:userId route
+app.get("/users/:userId", (req, res) => {
+  res.json({ ok: true, userId: req.userIdNum });
+});
+
+// Start the server by listening (Completion of TODO-1)
+app.listen(PORT, () => {
+  console.log(`API running at http://localhost:${PORT}`);
+});
